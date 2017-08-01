cf9946c9fa59b8898a87998adbbd7d8a5e45f596
liao66 <454452063@qq.com>
2017-08-01 09:47:54 +0800

    springboot环境搭建

---
+++ src/main/java/org/l/dbinto/DbintoApplication.java
@@ -0,0 +1,12 @@
+package org.l.dbinto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbintoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbintoApplication.class, args);
+	}
+}
---
+++ src/test/java/org/l/dbinto/DbintoApplicationTests.java
@@ -0,0 +1,16 @@
+package org.l.dbinto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DbintoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
